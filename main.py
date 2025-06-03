ded68411f40f8c70e57b48af0ca8106b7a02419f
aldodagio <103005119+aldodagio@users.noreply.github.com>
2025-06-02 20:13:16 -0700

    main - scrape kicking stats for one season

--- main.py
+++ main.py
@@ -23,37 +23,17 @@ def set_up_cleaner(path_to_csv, output_csv):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     week = 1
-    end_week = 19
-    year = 2024
+    end_week = 18 # end week will be week 19 after season gets extended
+    year = 2010
     db = Connection()
-    season_id = 15
-    pos = 'Wide Receiver'
+    season_id = 1
+    pos = 'K'
+    scraper = Scraper(year,week,'b6406b7aea3872d5bb677f064673c57f',pos)
     while week < end_week:
         root = 'C:/Users/aldod/PycharmProjects/fantasyfootball/data'
-        output_folder = '/clean_data/'
-        output_path = build_path(root + output_folder, year, 'WR', week)
-        with open(output_path, "r", newline="") as file:
-            reader = csv.reader(file)
-            next(reader)
-            for row in reader:
-                full_name = row[0].strip()
-                parts = full_name.split()
-                if len(parts) >= 2:
-                    first_name = parts[0]
-                    last_name = " ".join(parts[1:])
-                else:
-                    raise ValueError(f"Unexpected player name format: '{full_name}'")
-                player_id = db.select_player_id(first_name,last_name)
-                season_id = db.select_season_id(year)
-                game = row[2]
-                team = Team()
-                away_team_id = team.get_away_team_id(game, db)
-                home_team_id = team.get_home_team_id(game, db)
-                game_id = db.select_game_id(home_team_id, away_team_id, season_id)
-                total_points = row[3]
-                fumbles = row[18]
-                pass_id = db.select_pass_id(game_id, player_id)
-                rush_id = db.select_rush_id(game_id, player_id)
-                reception_id = db.select_reception_id(game_id, player_id)
-                db.insert_stats(pass_id, rush_id, reception_id, total_points, fumbles, game_id, player_id)
+        output_folder = '/raw_data/'
+        output_path = build_path(root + output_folder, year, 'K', week)
+        scraper.setWeek(week)
+        scraper.setURL()
+        scraper.scrape()
         week = week + 1
